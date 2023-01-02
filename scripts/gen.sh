6495fb0621f7bf39b5a87d1b7b6052d7b9d636ed
Matthew Tondreau <mmtondreau@gmail.com>
2023-01-02 08:51:32 -0800

    Enum subclass

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -eu
 
-datamodel-codegen --input tdaclient/schema/json --input-file-type jsonschema --output tdaclient/schema --enum-field-as-literal one 
+datamodel-codegen --input tdaclient/schema/json\
+  --input-file-type jsonschema\
+   --output tdaclient/schema\
+    --enum-field-as-literal one\
+    --use-subclass-enum\
+    --target-python-version 3.11
